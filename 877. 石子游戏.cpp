bc2590a4d038c2d74bf64c2fb7a14191904bdafd
Tscharrl <59212994+Tscharrl@users.noreply.github.com>
2020-03-26 02:01:40 +0800

    Update 877. 石子游戏.cpp

--- 877. 石子游戏.cpp
+++ 877. 石子游戏.cpp
@@ -1,53 +1,54 @@
 /*
-Leetcode面试题47 礼物的最大价值
+Leetcode1025 除数博弈
 
 Author:     Tscharrl
 Language:   C++
 Tag:        DP
 
 Description: 
-在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。
-你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。
-给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+亚历克斯和李用几堆石子在做游戏。偶数堆石子排成一行，每堆都有正整数颗石子 piles[i] 。
+游戏以谁手中的石子最多来决出胜负。石子的总数是奇数，所以没有平局。
+亚历克斯和李轮流进行，亚历克斯先开始。 每回合，玩家从行的开始或结束处取走整堆石头。 这种情况一直持续到没有更多的石子堆为止，此时手中石子最多的玩家获胜。
+假设亚历克斯和李都发挥出最佳水平，当亚历克斯赢得比赛时返回 true ，当李赢得比赛时返回 false 。
 
 Example: 
-    Input: [[1,3,1],
-            [1,5,1],
-            [4,2,1]]
-    Output: 12
-    Explain: 1->3->5->2->1
+    Input: [5,3,4,5]
+    Output: true
+    Explain: 亚历克斯先开始，只能拿前 5 颗或后 5 颗石子 。
+             假设他取了前 5 颗，这一行就变成了 [3,4,5] 。
+             如果李拿走前 3 颗，那么剩下的是 [4,5]，亚历克斯拿走后 5 颗赢得 10 分。
+             如果李拿走后 5 颗，那么剩下的是 [3,4]，亚历克斯拿走后 4 颗赢得 9 分。
+             这表明，取前 5 颗石子对亚历克斯来说是一个胜利的举动，所以我们返回 true 。
 */
 
 class Solution {
 public:
-    int maxValue(vector<vector<int> >& grid) {
-        int row = grid.size(), col = grid[0].size();
-        
-        // 二维动态规划
-        vector<vector<int> > value(row, vector<int>(col));
-        value[0][0] = grid[0][0];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (i == 0 && j == 0)
-                    value[i][j] = grid[0][0];
-                else if (i == 0)
-                    value[i][j] = value[i][j-1] + grid[i][j];
-                else if (j == 0)
-                    value[i][j] = value[i-1][j] + grid[i][j];
-                else
-                    value[i][j] = max(value[i][j-1], value[i-1][j]) + grid[i][j];
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        // score[i][j]: best score of piles[i] -> piles[j]
+        // Note: only upper triangular matrix will be upgraded (i must >= j)
+        vector<vector<int> > score(n, vector<int>(n));
+        // only left 1 pile
+        for (auto i = 0; i < n; i++) 
+            score[i][i] = piles[i];
+        // left 2 piles, must choose bigger one, always positive
+        for (auto i = 0; i < n-1; i++)
+            score[i][i+1] = abs(piles[i] - piles[i+1]);
+        // left more than 2 piles
+        // eg: ABC
+        //     choose A, got num(A) - score(BC)
+        //     choose C, got num(C) - score(AB)
+        //     ■■□□□□
+        //     □■■□□□
+        //     □□■■□□
+        //     □□□■■□ <- upgrade from here: [n - 3][i + 2]
+        //     □□□□■■
+        //     □□□□□■
+        for (auto i = n - 3; i >= 0; i--){
+            for (auto j = i + 2; j < n; j++) {
+                score[i][j] = max(piles[i]-score[i+1][j], piles[j]-score[i][j-1]);
             }
         }
-        return value[row-1][col-1];
-
-        // 一维动态规划
-        vector<int> value(col);
-        for (int i = 0; i < row; i++) {
-            value[0] = value[0] + grid[i][0];
-            for (int j = 1; j < col; j++) {
-                value[j] = max(value[j], value[j-1]) + grid[i][j];
-            }
-        }
-        return value[col-1];
+        return score[0][n-1] > 0;
     }
 };
